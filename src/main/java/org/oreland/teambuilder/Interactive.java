cb0eb941bb0c41210f3ea0638cf322319026ae52
Jonas Oreland <jonas.oreland@gmail.com>
2016-01-01 13:44:32 +0100

    start working on syncronize...

--- src/main/java/org/oreland/teambuilder/Interactive.java
+++ src/main/java/org/oreland/teambuilder/Interactive.java
@@ -5,7 +5,10 @@ import org.oreland.teambuilder.sync.Synchronizer.Specifier;
 import org.oreland.teambuilder.ui.Dialog;
 import org.oreland.teambuilder.ui.DialogBuilder;
 
+import java.io.FileOutputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -41,21 +44,61 @@ public class Interactive {
         }
     }
 
-    private void synchronize() {
+    private void synchronize() throws Exception {
+        ctx.myclub.init(ctx.prop);
+        ctx.myclub.login(ctx.prop, ctx.builder);
+        ctx.myclub.setupClub(ctx, ctx.prop, ctx.builder);
+        ctx.prop.store(new FileOutputStream("config.properties"), null);
+        Selection selection = makeSelection(ctx, ctx.myclub);
     }
 
     private void statistics() throws Exception {
-        Specifier section = DialogBuilder.selectOne(ctx.builder,
-                "Select section", ctx.csv.listSections(ctx));
-        ctx.csv.setSection(ctx, section);
-        List<Specifier> teams = DialogBuilder.selectMulti(ctx.builder,
-                "Select teams", ctx.csv.listTeams(ctx), true);
+        Selection selection = makeSelection(ctx, ctx.csv);
+    }
+
+    class Selection {
+        Selection() {}
+
+        Specifier section;
+        List<Specifier> teams;
+        List<Pair<Specifier, List<Specifier>>> periods; // Pair<Team, List<Period>>
+    }
+
+    private Selection makeSelection(Context ctx, Synchronizer sync) throws Exception {
+        Selection selection = new Selection();
+        selection.section = DialogBuilder.selectOne(ctx.builder,
+                "Select section", sync.listSections(ctx));
+        sync.setSection(ctx, selection.section);
+        selection.teams = DialogBuilder.selectMulti(ctx.builder,
+                "Select teams", sync.listTeams(ctx), true);
+        selection.periods = new ArrayList<>();
         List<Specifier> allperiods = new ArrayList<>();
-        for (Specifier team : teams) {
-            ctx.csv.setTeam(ctx, team);
-            allperiods.addAll(ctx.csv.listPeriods(ctx));
+        for (Specifier team : selection.teams) {
+            sync.setTeam(ctx, team);
+            Pair<Specifier,List<Specifier>> p = new Pair<>(team,
+                    sync.listPeriods(ctx));
+            selection.periods.add(p);
+            allperiods.addAll(p.second);
         }
-        List<Specifier> periods = DialogBuilder.selectMulti(ctx.builder,
-                "Select periods", allperiods, true);
+        HashSet<Specifier> selected_periods = new HashSet<>();
+        selected_periods.addAll(DialogBuilder.selectMulti(ctx.builder,
+                "Select periods", allperiods, true));
+
+        // remove those periods that were not selected
+        for (Pair<Specifier, List<Specifier>> period : selection.periods) {
+            // make copy of list, to avoid iterate/modify problem
+            List<Specifier> copy = new ArrayList<>();
+            copy.addAll(period.second);
+            period.second.clear();
+
+            for (Specifier p : copy) {
+                if (selected_periods.contains(p)) {
+                    System.out.println("team: " + period.first.name + ", period: " + p.name);
+                    period.second.add(p);
+                }
+            }
+        }
+
+        return selection;
     }
 }
